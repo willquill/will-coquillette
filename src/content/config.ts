b7cfb17cf2c7feb955e4a2e36004cd3484472e73
Will Coquillette <4646219+willquill@users.noreply.github.com>
2025-02-24 11:57:53 -0600

    remove hero image requirement from blogs
    Signed-off-by: Will Coquillette <4646219+willquill@users.noreply.github.com>

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,13 @@
-import { defineCollection, z, collection } from 'astro:content';
+import { z, defineCollection } from 'astro:content';
 
 const blogCollection = defineCollection({
   type: 'content',
   schema: ({ image }: { image: () => z.ZodType<any> }) =>
     z.object({
       title: z.string().max(80).min(10),
+      description: z.string().max(220).min(110),
       hero: image().optional(),
       heroAlt: z.string().optional(),
-      description: z.string().max(220).min(110),
       pubDate: z.date(),
       updatedDate: z.date().optional(),
       tags: z.array(z.string()),
